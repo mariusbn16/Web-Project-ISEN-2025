d2ce74270cdb6a3e952cd011883fc7cf6b8e0a8b
mariusbn16 <mariusbougouin@gmail.com>
2026-03-17 16:11:24 +0100

    Remove identification in constants.php

--- php/constants.php
+++ php/constants.php
@@ -1,8 +1,8 @@
 <?php
 
-    define('DB_USER', 'etu0505');
-    define('DB_PASSWORD', 'akgdjgss');
-    define('DB_NAME', 'etu0505');
-    define('DB_SERVER', 'localhost');
+    define('DB_USER', '');
+    define('DB_PASSWORD', '');
+    define('DB_NAME', '');
+    define('DB_SERVER', '');
 
 ?>
